41548377bc52221387520ba31b4d619936346006
Thiago Araujo <thiago_liveira@hotmail.com>
2021-03-16 22:06:52 -0300

    First test with Cypress and Mocha

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Cypress_Automation
- 
+
+https://learning.oreilly.com/videos/cypress-modern/9781839216480/
+
+npm run cy:run
+npm run cy:run-headed
---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,8 @@
+describe('Test Suite 1', function () {
+  it('Test Case 1', function () {
+    //Test Step
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    //fixture
+  })
+
+})
