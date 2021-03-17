0ad43e62db264e9512c34ee359448c1c2666c115
Thiago Araujo <thiago_liveira@hotmail.com>
2021-03-16 23:13:42 -0300

    Inserting appropriate names for tests

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,8 +1,8 @@
 //For visual Studio suggestions
 /// <reference types="Cypress" />
 
-describe('Test Suite 1', function () {
-  it('Test Case 1', function () {
+describe('Buying Products', function () {
+  it.skip('should have 4 visible products for the \'ca\' searching', function () {
     //Test Step
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
     //fixture will be learn in the future
@@ -11,7 +11,18 @@ describe('Test Suite 1', function () {
     cy.wait(2000)
     // This is not working , have 5 elements if consider just class product because one is invisible
     //cy.get('.product').should('have.length', 4)
+    //Checking the visible
     cy.get('.product:visible').should('have.length', 4)
+    //Chaining the child
+    cy.get('.products').find('.product').should('have.length', 4)
+
+  })
+
+  it('should add products for the cart', function () {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    cy.get('.products').find('.product').contains('Carrot - 1 Kg').should('have.text', 'Carrot - 1 Kg')
 
   })
 
