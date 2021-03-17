e0a43fc5020d1b39dbf70493f78cf4a5c4505a90
Thiago Araujo <thiago_liveira@hotmail.com>
2021-03-16 22:48:50 -0300

    Including first CSS Selector use

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Cypress_Automation
 
+Using Cypress + Mocha + Chai
+
+This code is from the lessons of:
 https://learning.oreilly.com/videos/cypress-modern/9781839216480/
 
 npm run cy:run
--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,8 +1,17 @@
+//For visual Studio suggestions
+/// <reference types="Cypress" />
+
 describe('Test Suite 1', function () {
   it('Test Case 1', function () {
     //Test Step
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
-    //fixture
+    //fixture will be learn in the future
+    cy.get('.search-keyword').type('ca')
+    //Should is a command from chai library
+    cy.wait(2000)
+    //cy.get('.product').should('have.length', 4)// This is not working , because have 5 elements if consider just class product
+    cy.get('.product[class="product"]').should('have.length', 4)
+
   })
 
 })
