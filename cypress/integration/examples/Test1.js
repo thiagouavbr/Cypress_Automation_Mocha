b9cd40aba3e1f6fea5586250bf150d5d652d176b
Thiago Araujo <thiago_liveira@hotmail.com>
2021-03-16 22:55:38 -0300

    Including the visible mode for searching

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -9,8 +9,9 @@ describe('Test Suite 1', function () {
     cy.get('.search-keyword').type('ca')
     //Should is a command from chai library
     cy.wait(2000)
-    //cy.get('.product').should('have.length', 4)// This is not working , because have 5 elements if consider just class product
-    cy.get('.product[class="product"]').should('have.length', 4)
+    // This is not working , have 5 elements if consider just class product because one is invisible
+    //cy.get('.product').should('have.length', 4)
+    cy.get('.product:visible').should('have.length', 4)
 
   })
 
